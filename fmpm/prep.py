f5586fad0a4869c182eb1d427366460bf9872781
Nida Janulaitis <nida@Nidas-MacBook-Pro.local>
2021-03-15 15:06:27 -0700

    updated prep.py documentation and plastics list

--- fmpm/prep.py
+++ fmpm/prep.py
@@ -1,8 +1,27 @@
+"""
+prep.py module for preparing 10X microscopy images of microparticles.
+
+Contains functions for preparing microscopy images of single 
+particles and corresponding data for machine learning with pytorch.
+Also contains dataset class for 10X microscopy data that builds on
+the pytorch Dataset class.
+
+Notes
+-----
+Microscopy images are limited to be 10X and single particle per image
+(.bmp format).
+Data must be cleaned and formatted correctly in a .csv
+(see examples and/or tests).
+Current plastics identified: 'polystyrene', 'polyethylene','polypropylene',
+'Nylon', 'ink + plastic','PET','carbon fiber','polyamide resin', and 'PVC'
+
+"""
+
 import copy
-import random
-import time
 import os
+import random
 import re
+import time
 
 import math
 import numpy as np
@@ -16,18 +35,37 @@ import torchvision.datasets
 
 
 def set_seeds(seed):
-    """sets seeds for several used packages for reproducibility"""
+    """
+    Set seeds for several used packages, for reproducibility.
+
+    Parameters
+    ----------
+    seed: int
+
+    Returns
+    -------
+    None
+    """
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
-    torch.backends.cudnn.deterministic = True
-    
+    torch.backends.cudnn.deterministic = True   
+    return None
     
 def encode_column(column):
     """
-    takes single columned Pandas DataFrame of categorical data and encodes it
-    into array of class binarys
+    Encode a colum of a Pandas DataFrame that contains categorical data into
+    and array of class binarys. 
+
+    Parameters
+    ----------
+    column: DataFrame column
+
+    Returns
+    -------
+    list of encodings 
+        List containts encoding for each row of the DataFrame. 
     """
     encoder = sklearn.preprocessing.OneHotEncoder()
     shape_arr = encoder.fit_transform(column).toarray().astype(int)
@@ -35,6 +73,19 @@ def encode_column(column):
     return list(shape_arr)
 
 def remove_nones(df):
+    """
+    Removes rows of DataFrame that contain no file name in 'File' column.
+
+    Parameters
+    ----------
+    df: DataFrame
+        DataFrame should have a 'File' column
+    
+    Returns
+    -------
+    DataFrame
+        Updated DataFrame without Nones in column 'File'
+    """
     for index, row in df.iterrows():
         if row['File'] == None:
             df.drop(index, inplace=True)
@@ -44,7 +95,23 @@ def remove_nones(df):
 
 def add_filenames(labels, image_root):
     """
-    Replaces sample column of labels with the actual filename so that the dataset class doesn't have to do that work.
+    Adds column to DataFrame titled 'File' that contains the file name 
+    corresponding to the sample identification. 
+    
+    Parameters
+    ----------
+    labels: DataFrame
+        DataFrame has a 'Sample' column containing sample identification
+        that corresponds to a file name in image_root.
+    image_root: str
+        Path to directory where files are located. 
+
+    Returns
+    -------
+    DataFrame
+        DataFrame has an additonal column titled 'File' that contains
+        the name of the corresponding file if it exists. If no matching file
+        is found, the value for that row is None. 
     """
     image_filenames = os.listdir(image_root)
     labels.insert(loc=1, column='File', value=None)
@@ -66,12 +133,40 @@ def add_filenames(labels, image_root):
 
 
 def split_sample(labels):
+    """
+    Split the 'Sample' column of a DataFrame into a list.
+
+    Parameters
+    ----------
+    labels: DataFrame
+        The Dataframe should contain a 'Sample' column for splitting.
+
+    Returns
+    -------
+    DataFrame
+        Updated DataFrame has 'Sample' column with a list of strings. 
+    """
     sample_names = labels["Sample"].str.split(" ", n=1, expand=False)
     labels['Sample'] = sample_names
     return labels
     
 
 def split_description(labels):
+    """
+    Split the 'Description' column of a DataFrame into 'Color' and 'Shape'
+    columns. 
+
+    Parameters
+    ----------
+    labels: DataFrame
+        DataFrame conaining a 'Desription column' with space-separated strings
+        corresponding to color and shape of the sample.
+
+    Returns
+    -------
+    DataFrame
+        The updated DataFrame contains a 'Shape' column and 'Color' column. 
+    """
     new = labels["Description"].str.split(" ", n=1, expand=True)
     labels.drop(columns=['Description'], inplace=True)
     labels['Color'] = new[0].values
@@ -80,7 +175,24 @@ def split_description(labels):
     
     
 def convert_plastics(labels):
-    PLASTICS = ['polystyrene', 'polyethylene','polypropylene','Nylon','ink + plastic','PET','carbon fiber']
+    """
+    Convert 'Identification' column of a DataFrame with an 'isPlastic' column
+    corresponding to whether the sample is plastic (1) or not (0). 
+
+    Parameters
+    ----------
+    labels: DataFrame
+        DataFrame containing an 'Identification' column with known
+        identifications as strings.
+
+    Returns
+    -------
+    DataFrame
+        The updated DataFrame contains a column 'isPlastic' which
+        contains 1 if the sample is platic and 0 if it is not. 
+    """
+    PLASTICS = ['polystyrene', 'polyethylene','polypropylene','Nylon',
+                'ink + plastic','PET','carbon fiber','polyamide resin', 'PVC']
     identification = labels['Identification']
     
     for i in range(0,len(identification)):
@@ -97,8 +209,22 @@ def convert_plastics(labels):
 
 def prep_data(labels, image_root):
     """
-    Takes in raw labels dataframe and converts it into the format
-    expected for tenX_dataset class
+    Prepares raw labels DataFrame and converts it into the format
+    expected for tenX_dataset class.
+
+    Parameters
+    ----------
+    labels: DataFrame
+        Raw DataFrame, read from a csv containing a cleaned and 
+        formatted dataset. 
+
+    Returns
+    -------
+    DataFrame
+        DataFrame updated for loading into a tenX_dataset class.
+        Sample labels are split to a list.
+        'File', 'Shape', 'Color', and 'isPLastic' columns have
+        been added. 
     """
     labels = split_description(labels)
     labels = split_sample(labels)
@@ -116,19 +242,46 @@ def prep_data(labels, image_root):
 
 class tenX_dataset(torch.utils.data.Dataset):
     """
+    Dataset class for 10X miscroscopy images of individual particulates. 
+
     Class inherited from torch Dataset. Required methods are, init,
     len, and getitem.
+    
+    Attributes
+    ----------
+    labels: DataFrame
+        DataFrame containing all data labels.
+    image_dir: str
+        Path to directory where the images are located.
+    image_filenames: list
+        A list of all image file names in the image folder.
+    transform: pytorch object
+        Pytorch object containing all of the defined transforms
+        for the images.
     """
     def __init__(self, labels_frame, image_dir, transform = None):
         """
-        initializes an instance of the class. Here we store 4 variables
-        in the class. Calling init just looks like dataset = tenX_dataset(lables, 'image_folder', transform).
+        Initializes an instance of the class. 
         
-        labels: altered version of csv file
-        image_dir: The file path to the folder the images are in
-        image_filenames: A list of all the image file names in the image folder
-        transform: A pytorch object. Works like a function. You call transform(x) and it performs
-                    a series of operations on x
+        During initializatio 4 variables are stored in the class. 
+
+        Parameters
+        ----------
+        labels: DataFrame
+            Updated DataFrame prepared with prep_data function.
+        image_dir: str
+            The file path to the folder the images are in.
+        image_filenames: list of str
+            A list of all the image file names in the image folder.
+        transform: pytorch object
+            Works like a function. Call transform(x) and to perform
+            a series of operations on x.
+
+        Examples
+        --------
+        Calling init looks like:
+        dataset = tenX_dataset(labels, 'image_folder', transform).
+        
         """
         self.labels = labels_frame
         self.image_dir = image_dir
@@ -137,15 +290,30 @@ class tenX_dataset(torch.utils.data.Dataset):
         
 
     def __len__(self):
-        """Returns the length of the dataset"""
+        """
+        Return the length of the dataset.
+        
+        Returns
+        -------
+        int
+            Length of the dataset (number of samples).
+        """
         return len(self.labels)
     
     
     def __getitem__(self, idx):
         """
-        Returns a dictionary containing image and image data. Right now
-        it looks like: 
-        sample = {'image': image, 'plastic': [0], 'shape':[0,0,0,0,0], 'color':[0,0,0,0,0]}
+        Return a dictionary containing image and image data.
+        
+        Returns
+        -------
+        dict
+            Dictionary containing image imformation.
+
+        Examples
+        --------
+        sample = {'image': image, 'plastic': [0],
+                 'shape':[0,0,0,0,0], 'color':[0,0,0,0,0]}
         """
         image_filename = self.labels['File'][idx]
         image = None
